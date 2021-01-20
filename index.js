ad49bb6609296860f19657a7bea8dbc3cb766608
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-19 19:14:22 -0800

    all passing

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => {
+    return driver.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch(drivers, letters) {
+return drivers.filter(driver => {
+  return driver.startsWith(letters)
+})
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => {
+    return driver.name === name
+  })
+}
